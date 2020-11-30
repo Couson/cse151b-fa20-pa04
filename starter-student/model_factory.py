832fd1829d3fad88e587933b9c80e2bbfc3f8bba
RamboShi <36247763+RamboShi@users.noreply.github.com>
2020-11-30 03:08:28 -0800

    Update model_factory.py

--- starter-student/model_factory.py
+++ starter-student/model_factory.py
@@ -5,6 +5,8 @@
 ################################################################################
 
 # Build and return the model here based on the configuration.
+
+from torch.nn import Embedding, LSTM
 def get_model(config_data, vocab):
     hidden_size = config_data['model']['hidden_size']
     embedding_size = config_data['model']['embedding_size']
@@ -14,5 +16,8 @@ def get_model(config_data, vocab):
     resnet50 = models.resnet50(pretrained = True)
     resnet50.fc=nn.Linear(2048, embedding_size)
     
+    
+    embed = Embedding(len(vocab), embedding_size)
+    lstm = LSTM(input_size=embedding_size, hidden_size=hidden_size, num_layers = 1, batch_first=True)
     
     raise NotImplementedError("Model Factory Not Implemented")
