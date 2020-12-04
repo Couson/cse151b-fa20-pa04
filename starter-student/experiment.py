aa9bd288a751213c479ad9f7cf91757719238862
RamboShi <yus263@ucsd.edu>
2020-12-04 14:32:48 -0800

    1

--- starter-student/experiment.py
+++ starter-student/experiment.py
@@ -189,7 +189,7 @@ class Experiment(object):
                 if bleu4(caption_word_list, predicted_word_list) < 6 and bads <= 10:
                     print(img_ids[j], caption_word_list, predicted_word_list)
                     bads += 1
-                elif bleu4(caption_word_list, predicted_word_list) > 9 and goods <= 10:
+                elif bleu4(caption_word_list, predicted_word_list) > 10 and goods <= 10:
                     print(img_ids[j], caption_word_list, predicted_word_list)
                     goods += 1
                     
